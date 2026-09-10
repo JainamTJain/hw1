e0ddcd9664647cce390ffb90bfc61d9f5e6258c4
JainamTJain <jjainam2711@gmail.com>
2026-09-10 09:03:03 -0700

    Implement split and ULListStr changes w push/pop/front/back function

--- split.cpp
+++ split.cpp
@@ -11,13 +11,36 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
+static void splitHelper(Node* in, Node** oddsTail, Node** evensTail);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
+  splitHelper(in, &odds, &evens);
+  in = NULL;
 // WRITE YOUR CODE HERE
 }
 
 /* If you needed a helper function, write it here */
+static void splitHelper(Node* in, Node** oddsTail, Node** evensTail)
+{
+  if (in == NULL) {
+    *oddsTail = NULL;
+    *evensTail = NULL;
+    return;
+  }
+
+  Node* rest = in->next;   // save before we touch in->next
+  in->next = NULL;         // detach this node from the rest
+
+  if (in->value % 2 != 0) {
+    *oddsTail = in;                              // attach here
+    splitHelper(rest, &(in->next), evensTail);    // next odd attaches after this node
+  } else {
+    *evensTail = in;
+    splitHelper(rest, oddsTail, &(in->next));
+  }
+}
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,134 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+void ULListStr::push_back(const std::string& val)
+{
+  if (tail_ == NULL) {
+    // list is empty -- allocate first node
+    Item* newItem = new Item();
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+    head_ = tail_ = newItem;
+  } else if (tail_->last < ARRSIZE) {
+    // room at the end of tail_'s array
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+  } else {
+    // tail_ is full -- allocate a new node after it
+    Item* newItem = new Item();
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+    newItem->prev = tail_;
+    tail_->next = newItem;
+    tail_ = newItem;
+  }
+  size_++;
+}
+
+void ULListStr::pop_back()
+{
+  if (tail_ == NULL) {
+    throw std::invalid_argument("pop_back on empty list");
+  }
+
+  tail_->last--;
+
+  if (tail_->first == tail_->last) {
+    // node is now empty -- deallocate it
+    Item* toDelete = tail_;
+    tail_ = tail_->prev;
+    if (tail_ != NULL) {
+      tail_->next = NULL;
+    } else {
+      head_ = NULL; // list is now empty
+    }
+    delete toDelete;
+  }
+  size_--;
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+  if (head_ == NULL) {
+    // list is empty, we need to allocate first node
+    Item* newItem = new Item();
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+    head_ = tail_ = newItem;
+  } else if (head_->first > 0) {
+    // room before 'first' in head_'s array
+    head_->first--;
+    head_->val[head_->first] = val;
+  } else {
+    // head_ has no room before 'first' -- allocate a new head node
+    // place value at the last slot, leaving room before it for future push_fronts
+    Item* newItem = new Item();
+    newItem->first = ARRSIZE - 1;
+    newItem->last = ARRSIZE;
+    newItem->val[ARRSIZE - 1] = val;
+    newItem->next = head_;
+    head_->prev = newItem;
+    head_ = newItem;
+  }
+  size_++;
+}
+
+void ULListStr::pop_front()
+{
+  if (head_ == NULL) {
+    throw std::invalid_argument("pop_front on empty list");
+  }
+
+  head_->first++;
+
+  if (head_->first == head_->last) {
+    // node is now empty -- deallocate it
+    Item* toDelete = head_;
+    head_ = head_->next;
+    if (head_ != NULL) {
+      head_->prev = NULL;
+    } else {
+      tail_ = NULL; // list is now empty
+    }
+    delete toDelete;
+  }
+  size_--;
+}
+
+std::string const & ULListStr::back() const
+{
+  if (tail_ == NULL) {
+    throw std::invalid_argument("back on empty list");
+  }
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const
+{
+  if (head_ == NULL) {
+    throw std::invalid_argument("front on empty list");
+  }
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  if (loc >= size_) return NULL;
+
+  Item* cur = head_;
+  size_t count = 0;
+
+  while (cur != NULL) {
+    size_t used = cur->last - cur->first;
+    if (loc < count + used) {
+      return const_cast<std::string*>(&cur->val[cur->first + (loc - count)]);
+    }
+    count += used;
+    cur = cur->next;
+  }
+  return NULL; // I would say shouldn't be reached if size_ is maintained correctly
+}
